e4e3bd592df6727e2051e78f3ed828fbf53cb60b
BoholmanIT <boholman.it.tech.nstu@gmail.com>
2024-10-22 01:04:25 +0700

    Finish stage programm

--- sel.py
+++ sel.py
@@ -16,4 +16,11 @@ browser.find_element("xpath", "/html/body/div[2]/div/main/div[4]/div[1]/div/div[
 time.sleep(3)
 browser.find_element("xpath", "/html/body/div[2]/div/main/div[8]/a[3]").click()
 time.sleep(3)
+print(browser.current_window_handle)
+browser.switch_to.window(browser.window_handles[1])
+print(browser.current_window_handle)
+time.sleep(3)
+for i in range(2, 102):
+    info = browser.find_element("xpath", f"/html/body/div[2]/div/main/div[10]/div[2]/div[{i}]")
+    print(f"{info.text}")
 browser.close()
