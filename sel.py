22843ee80a14784721632efb08ff61ba53c61f58
BoholmanIT <boholman.it.tech.nstu@gmail.com>
2024-10-21 14:13:01 +0700

    make webdriver and link nstu

--- sel.py
+++ sel.py
@@ -1,3 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.support.select import select
 import time
+
+browser = webdriver.Chrome()
+browser.maximize_window()
+browser.get("https://nstu.ru/")
+time.sleep(5)
