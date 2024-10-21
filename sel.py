b89edb7f319b3eefa302a09bfd5c39f24ac58a6b
BoholmanIT <boholman.it.tech.nstu@gmail.com>
2024-10-21 19:38:19 +0700

    added a transition to the table

--- sel.py
+++ sel.py
@@ -1,7 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.support import select
 import time
 
 browser = webdriver.Chrome()
 browser.maximize_window()
 browser.get("https://nstu.ru/")
-time.sleep(5)
+time.sleep(10)
+browser.find_element("xpath", "/html/body/div[2]/header/div[1]/div[2]/div[2]/div[2]/nav/div[1]/a").click()
+time.sleep(3)
+browser.find_element("xpath", "/html/body/div[2]/div/main/div[4]/a[8]").click()
+time.sleep(3)
+browser.find_element("xpath", "/html/body/div[2]/div/main/div[4]/div[1]/a").click()
+time.sleep(3)
+browser.find_element("xpath", "/html/body/div[2]/div/main/div[4]/div[1]/div/div[4]/div[4]/a").click()
+time.sleep(3)
+element = browser.find_element("xpath", "/html/body/div[2]/div/main/div[10]/div[2]")
+print(f"I show - {element}")
